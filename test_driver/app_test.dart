7f4eaa26163ab37b6ecc8f8876b944986be22a11
kazuikios <slash0juice@gmail.com>
2021-06-30 00:27:08 +0900

    add Integration test

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -1,19 +1,17 @@
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
-void main(){
-
+void main() {
   late FlutterDriver driver;
 
   setUpAll(() async {
     driver = await FlutterDriver.connect();
     final health = await driver.checkHealth();
-    if ( health.status == HealthStatus.bad) {
+    if (health.status == HealthStatus.bad) {
       fail("Flutter driver 起動失敗");
     }
   });
-  tearDownAll(() async {
-  });
+  tearDownAll(() async {});
 
   final SerializableFinder key1Finder = find.text("1");
 
